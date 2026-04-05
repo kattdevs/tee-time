f8d228808c943eb06f73b6f6f16b74e8ee9cf8bb
Katlego Twala <twalakatlego44@gmail.com>
2026-04-05 17:43:42 +0200

    enables Redux DevTools in development

---
+++ tee-time/client/src/store/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@@reduxjs/toolkit';
+import bookingReducer from './bookingSlice';
+import uiReducer from './uiSlice';
+
+//configureStore automatically enables Redux DevTools in development
+export const store = configureStore({
+    reducer: {
+        booking: bookingReducer,
+        ui: uiReducer,
+    },
+});
+
+export default store;
