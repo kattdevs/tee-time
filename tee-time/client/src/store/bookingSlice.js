a828c55aa3b2e47ecfdf511269c4365464038953
Katlego Twala <twalakatlego44@gmail.com>
2026-04-07 21:06:31 +0200

    set default to selected date

--- tee-time/client/src/store/bookingSlice.js
+++ tee-time/client/src/store/bookingSlice.js
@@ -47,7 +47,7 @@ const bookingSlice = createSlice({
     slots: [],
     reservations: [],
     selectedCourse: null,
-    selectedDate: '',
+    selectedDate: new Date().toISOString().split('T')[0],
     lastBooking: null,
     loading: false,
     error: null,
